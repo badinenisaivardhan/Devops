55a95a502891fab0aeea8b10dd17ccb00743a445
Badineni Sai Vardhan <badinenisaivardhan@gmail.com>
2022-12-17 19:36:01 +0530

    Add files via upload

---
+++ runfile.sh
@@ -0,0 +1,6 @@
+yum install unzip
+wget https://raw.githubusercontent.com/badinenisaivardhan/Devops/GNSS/ProjectETH.zip
+unzip ProjectETH.zip
+cd ProjectETH
+chmod +777 docker-run.sh
+./docker-run.sh
